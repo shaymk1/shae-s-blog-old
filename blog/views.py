4a084d2bc1e1160ce71bc86eb246c2cf7592b9d4
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-05 14:06:31 +0200

    created templates and configured them as well as the statics

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    model = Post
+    posts = Post.objects.all().order_by("-created_at")
+    context = {"posts": posts, "model": model}
+    return render(request, "blog/index.html", context)
+
+
+def post(request):
+    return render(request, "blog/post.html")
+
+
+def posts(request):
+    return render(request, "blog/posts.html")
--- core/settings/base_settings.py
+++ core/settings/base_settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -108,7 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
