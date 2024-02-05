43a7c8a53160b42ad79c9c29e7c4d657cef8796c
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-05 13:27:11 +0200

    created some files for pytest configuration

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,9 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="post_author"
     )
+
+    class Meta:
+        ordering = ("-created_at",)  # decending order
+
+    def __str__(self):
+        return self.title
